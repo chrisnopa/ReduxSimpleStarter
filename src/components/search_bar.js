68b0b9cb1488c35dbc35f0e99126c6c102a16b47
christophe panon <panon_chr@hotmail.com>
2018-05-27 18:24:21 -0600

    changed component

---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+// import React, {Component} from "react";
+
+
+// class SearchBar extends Component{
+//  render() {
+//   return <input onChange={this.handlerInputChange} />
+//  }
+
+//  handlerInputChange(event) {
+//   console.log(event.target.value)
+//  }
+// }
+
+
+// export default SearchBar;
+
+
+import React, {Component} from "react";
+
+
+class SearchBar extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {term: };
+  }
+ render() {
+  return <input onChange={event => console.log(event.target.value)} />
+ }
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+import SearchBar from './components/search_bar';
+
 const App = () => {
-  return <div>Salut les gars!</div>;
+  return (<div>
+    <SearchBar/>
+  </div>);
 }
 ReactDOM.render(<App/>,document.querySelector('.container'))
